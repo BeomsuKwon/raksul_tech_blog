c31a5a37a7b29ddbc6085b3cc02c6b0810dae02a
JUNSU JANG <jyunsu.jang@gmail.com>
2018-12-17 10:17:31 +0900

    initialize

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-row>
+      <el-button icon="el-icon-search" circle @click="auth()"> {{button}} </el-button>
+  </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      button : "login"  
+    }
+  },
+  methods : {
+    auth () {
+      alert("test")
+    }
+  }
+}
+
+</script>
